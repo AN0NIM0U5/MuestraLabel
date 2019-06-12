ee1a34b4418be9538fa9dbe038a49b60e7fcdb49
Alberto Fernandez Ramos <a695020809@hotmail.com>
2019-06-12 20:39:11 +0200

    Initial commit

---
+++ app/src/main/java/com/alb/muestralabel/MainActivity.java
@@ -0,0 +1,53 @@
+package com.alb.muestralabel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.DragAndDropPermissions;
+import android.view.DragEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+
+import org.w3c.dom.Text;
+
+import java.lang.annotation.Target;
+
+public class MainActivity extends AppCompatActivity {
+private Button btnEntry;
+private TextView textEntry;
+private EditText txt;
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEntry= findViewById(R.id.boton);
+        //txt =(TextView) findViewById(R.id.textoDevuelta);
+        textEntry= findViewById(R.id.textoDevuelta);
+
+        cambiaTexto();
+
+
+    }
+
+
+    private void cambiaTexto(){
+
+
+        btnEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textEntry.setText("sdhbgsdgd");
+            }
+        });
+
+        }
+
+
+    }
+
+
