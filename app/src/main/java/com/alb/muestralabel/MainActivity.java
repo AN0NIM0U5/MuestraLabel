d24f5645829e0ad61b3784a6cd1cbd519b634c53
Alberto Fernandez Ramos <a695020809@hotmail.com>
2019-06-13 19:20:43 +0200

    colour apps activity 3

--- app/src/main/java/com/alb/muestralabel/MainActivity.java
+++ app/src/main/java/com/alb/muestralabel/MainActivity.java
@@ -3,8 +3,10 @@ package com.alb.muestralabel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.DragAndDropPermissions;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,26 +31,63 @@ private EditText txt;
         //txt =(TextView) findViewById(R.id.textoDevuelta);
         textEntry= findViewById(R.id.textoDevuelta);
 
+        /// Vibracion boton
+    Button customBtn = ( Button ) findViewById( R.id.boton );
+    customBtn.setOnTouchListener( new CustomHapticListener( 100 ) );
+
+
         cambiaTexto();
 
 
     }
 
+//////
 
+
+    ////
     private void cambiaTexto(){
 
 
         btnEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textEntry.setText("sdhbgsdgd");
+                textEntry.setText("");//StringText
 
             }
         });
 
         }
+//////////////
+
+    private class CustomHapticListener implements View.OnTouchListener {
+
+        // Duration in milliseconds to vibrate
+        private final int durationMs;
+
+        public CustomHapticListener( int ms ) {
+            durationMs = ms;
+        }
 
 
+
+
+
+
+
+
+        /////////
+        @Override
+        public boolean onTouch( View v, MotionEvent event ) {
+            if( event.getAction() == MotionEvent.ACTION_DOWN ){
+                Vibrator vibe = (Vibrator) getSystemService( VIBRATOR_SERVICE );
+                vibe.vibrate( durationMs );
+
+
+
+            }
+            return true;
+        }
+    }
     }
 
 
